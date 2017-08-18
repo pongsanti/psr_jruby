df7676bd577d9e6974ed8a1b8d0e1a59b003c3a3
pongsanti <pongsanti.tanvejsilp@gmail.com>
2017-08-18 15:01:57 +0700

    logger, namespace, json

--- src/index.rb
+++ src/index.rb
@@ -1,7 +1,23 @@
 require 'sinatra'
-require "sinatra/json"
+require 'sinatra/custom_logger'
+require 'sinatra/json'
+require 'sinatra/namespace'
+require "sinatra/reloader" if development?
+require 'logger'
+require 'json'
 
-post '/api/public/login' do
+set :logger, Logger.new(STDOUT)
+
+post '/login' do
+  logger.info "logging in..."
   data = request.body.read
   json data
+end
+
+namespace '/api' do
+  post '/users' do
+    logger.info "creating user..."
+    payload = JSON.parse(request.body.read)
+    puts payload["username"]
+  end
 end
