aa2c18365f7bf6babeefb6839896c05d9faf6b83
pongsanti <pongsanti.tanvejsilp@gmail.com>
2017-08-19 13:37:53 +0700

    find user by username spec

--- spec/db/operation_spec.rb
+++ spec/db/operation_spec.rb
@@ -3,8 +3,19 @@ require 'db/operation'
 include DbOp
 
 describe DbOp do
+  around(:each) do |example|
+    DB.transaction(rollback: :always, auto_savepoint: true) {example.run}
+  end
+
   it 'should find user by username' do
-    user = db_find_user('patima.key@gmail.com')
+    User.new(username: 'joe', password: 'xxx').save
+
+    user = db_find_user('joe')
+    expect(user).to_not be_nil
+  end
+
+  it 'should not find user by username' do
+    user = db_find_user('mockuser')
     expect(user).to be_nil
   end
 end
