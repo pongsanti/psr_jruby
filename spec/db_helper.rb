5ff03b2f349aacabe17359c0e1b7bb67b121c201
pongsanti <pongsanti.tanvejsilp@gmail.com>
2017-08-23 11:27:57 +0700

    login spec

--- spec/db_helper.rb
+++ spec/db_helper.rb
@@ -1,12 +1,16 @@
 module SmartTrack::Test
   module Helper
-    def create_user(username)
-      user = SmartTrack::User.new(username: username, password: 'xxx').save
+    def create_user(username, password = 'xxx')
+      user = SmartTrack::User.new(username: username, password: password).save
+    end
+
+    def create_session(user, session_token)
+      DB.insert_user_session(user, session_token)
     end
 
     def create_user_session(username, session_token)
       user = create_user(username)
-      DB.insert_user_session(user, session_token)
+      create_session(user, session_token)
     end
   end
 end
---
+++ spec/routes/login_spec.rb
@@ -0,0 +1,61 @@
+require 'rack/test'
+require 'controller'
+require 'password'
+require_relative '../db_helper'
+
+describe 'SmartTrack' do
+  include Rack::Test::Methods
+  include SmartTrack::Test::Helper
+
+  def app
+    Sinatra::Application
+  end
+
+  around(:each) do |example|
+    DB.db.transaction(rollback: :always, auto_savepoint: true) {example.run}
+  end
+
+  username = 'john@gmail.com'
+  password = '1234'
+  request = {username: username, password: password}.to_json
+
+  context 'in unauthorized user context' do
+    it 'logging user in and return token' do
+      # prepare
+      create_user(username, create_password(password))
+      # execute
+      post_with_json '/login', request
+      
+      expect(last_response.status).to eq(200)
+      expect(last_response.body).to include('token')
+    end
+  end
+
+  context 'in authorized user context' do
+    token = 'mocktoken'
+    it 'logging user with new token' do
+      # prepare
+      user = create_user(username, create_password(password))
+      create_session(user, token)
+      # execute
+      post_with_json '/login', request
+
+      expect(last_response.status).to eq(200)
+      expect(last_response.body).to include('token')
+      new_token_return? token
+    end
+
+    def new_token_return? old_token
+      json_res = JSON.parse(last_response.body)
+      expect(json_res['token']).not_to eq(old_token)
+    end
+  end
+
+end
+
+def post_with_json(uri, json)
+  # No assertion in this, we just demonstrate how you can post a JSON-encoded string.
+  # By default, Rack::Test will use HTTP form encoding if you pass in a Hash as the
+  # parameters, so make sure that `json` below is already a JSON-serialized string.
+  post(uri, json, { 'CONTENT_TYPE' => 'application/json' })
+end
