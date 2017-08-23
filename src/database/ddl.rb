e32672941843b0a7ae08141c9afcdfc26278b0f8
pongsanti <pongsanti.tanvejsilp@gmail.com>
2017-08-23 15:52:19 +0700

    ddl script has environment, drop and seed data

--- src/database/ddl.rb
+++ src/database/ddl.rb
@@ -1,9 +1,8 @@
 require 'sequel'
-require 'mysql-connector-java-5.1.42-bin'
 
 HOST = 'localhost'
 PORT = '3306'
-DATABASE_NAME = 'sts_test'
+DATABASE_NAME = ENV["SINATRA_ENV"] == 'test' ? 'sts_test' : 'sts'
 
 DB = Sequel.connect("jdbc:mysql://#{HOST}:#{PORT}/#{DATABASE_NAME}?user=root&password=root&charset=utf8")
 
@@ -14,10 +13,15 @@ DB.quote_identifiers = false
 
 create_table_options = {charset: 'utf8'}
 
+# Drop tables
+DB << 'DROP TABLE user_sessions'
+DB << 'DROP TABLE users'
+
+# Create tables
 DB.create_table!(:users, create_table_options) do
   primary_key :id
-  String :username
-  String :password
+  String :email, null: false, unique: true
+  String :password, null: false
   String :display_name
   DateTime :created_at
   DateTime :updated_at
@@ -26,9 +30,14 @@ end
 DB.create_table!(:user_sessions, create_table_options) do
   primary_key :id
   foreign_key :user_id, :users
-  String :token
+  String :token, null: false
+  DateTime :expired_at, null: false
   DateTime :created_at
   DateTime :updated_at
 end
+
+# Seed data
+DB['INSERT INTO users (email, password, display_name) VALUES (?, ?, ?)',
+  'patima.key@gmail.com', '$2a$10$EiouwUDIvB4ygT5hK2NAG.dAcZuYnETMBKHgbae44oP11unaAjZnS', 'Patima'].insert
 
 DB.disconnect
