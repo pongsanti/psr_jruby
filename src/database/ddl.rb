f775158303977cd53a3c16d383e4ea2a6df3cd50
pongsanti <pongsanti.tanvejsilp@gmail.com>
2017-08-30 23:31:30 +0700

    revise ddl for rom

--- src/database/ddl.rb
+++ src/database/ddl.rb
@@ -1,43 +1,35 @@
-require 'sequel'
+require 'rom'
 
-HOST = 'localhost'
-PORT = '3306'
-DATABASE_NAME = ENV["SINATRA_ENV"] == 'test' ? 'sts_test' : 'sts'
+module SmartTrack
+  module Database
+    HOST = 'localhost'
+    PORT = '3306'
+    DATABASE_NAME = ENV["SINATRA_ENV"] == 'test' ? 'sts_test' : 'sts'
+    DB_URL = "jdbc:mysql://#{HOST}:#{PORT}/#{DATABASE_NAME}?user=root&password=root&charset=utf8"
 
-DB = Sequel.connect("jdbc:mysql://#{HOST}:#{PORT}/#{DATABASE_NAME}?user=root&password=root&charset=utf8")
+    @rom = ROM.container(:sql, DB_URL) do |conf|
+      conf.default.drop_table(:user_sessions, :users)
 
-DB.extension :identifier_mangling
-DB.identifier_input_method = nil
-DB.identifier_output_method = nil
-DB.quote_identifiers = false
+      conf.default.create_table(:users) do
+        primary_key :id
+        String :email, null: false, unique: true
+        String :password, null: false
+        String :display_name
+        DateTime :created_at
+        DateTime :updated_at
+      end
 
-create_table_options = {charset: 'utf8'}
+      conf.default.create_table(:user_sessions) do
+        primary_key :id
+        foreign_key :user_id, :users
+        String :token, null: false
+        DateTime :expired_at, null: false
+        DateTime :created_at
+        DateTime :updated_at        
+      end
 
-# Drop tables
-DB << 'DROP TABLE user_sessions'
-DB << 'DROP TABLE users'
-
-# Create tables
-DB.create_table!(:users, create_table_options) do
-  primary_key :id
-  String :email, null: false, unique: true
-  String :password, null: false
-  String :display_name
-  DateTime :created_at
-  DateTime :updated_at
+      conf.default.connection['INSERT INTO users (email, password, display_name) VALUES (?, ?, ?)',
+        'patima.key@gmail.com', '$2a$10$EiouwUDIvB4ygT5hK2NAG.dAcZuYnETMBKHgbae44oP11unaAjZnS', 'Patima'].insert
+    end
+  end
 end
-
-DB.create_table!(:user_sessions, create_table_options) do
-  primary_key :id
-  foreign_key :user_id, :users
-  String :token, null: false
-  DateTime :expired_at, null: false
-  DateTime :created_at
-  DateTime :updated_at
-end
-
-# Seed data
-DB['INSERT INTO users (email, password, display_name) VALUES (?, ?, ?)',
-  'patima.key@gmail.com', '$2a$10$EiouwUDIvB4ygT5hK2NAG.dAcZuYnETMBKHgbae44oP11unaAjZnS', 'Patima'].insert
-
-DB.disconnect
