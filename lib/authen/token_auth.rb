9051c64820a6a7eab12c94a1387a77179f31cb3c
pongsanti <pongsanti.tanvejsilp@gmail.com>
2017-09-02 00:36:21 +0700

    token authorization uses repos

--- lib/authen/token_auth.rb
+++ lib/authen/token_auth.rb
@@ -3,6 +3,14 @@ require_relative 'unauth_error'
 
 module SmartTrack
   module TokenAuth
+    def user_repo
+      SmartTrack::Database::Container.resolve(:user_repo)
+    end
+
+    def session_repo
+      SmartTrack::Database::Container.resolve(:session_repo)
+    end
+
     def token env
       env['HTTP_X_AUTHORIZATION']
     end
@@ -11,9 +19,10 @@ module SmartTrack
       token = token(env) || ''
       raise UnAuthError, "Unauthenticated request" if not token || token.empty?
       
-      user_session = DB.find_user_session(token)
+      user_session = session_repo.find_by_token(token)
       if user_session
-        @user = DB.find_user_by_id(user_session.user_id)
+        #@user = user_repo.query_first(id: user_session.user_id)
+        @user = user_session.user
       else
         raise UnAuthError, "Unauthenticated request"
       end
