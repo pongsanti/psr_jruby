10b92cd32a5799a45bd15e7410f4f9132341ebe6
pongsanti <pongsanti.tanvejsilp@gmail.com>
2017-09-02 00:32:00 +0700

    isolate setup code in function, repos into container

--- lib/controller.rb
+++ lib/controller.rb
@@ -1,3 +1,8 @@
+def setup
+
+end
+
+
 require 'sinatra'
 require 'sinatra/config_file'
 require 'sinatra/custom_logger'
@@ -18,26 +23,34 @@ require_relative 'password'
 
 enable :logging
 disable :show_exceptions
-
 config_file File.expand_path('config/sinatra.yml')
 
-db_url = "jdbc:mysql://#{settings.db_host}:\
+# setup database, container
+def setup
+  db_url = "jdbc:mysql://#{settings.db_host}:\
 #{settings.db_port}/\
 #{settings.db_name}?user=root&password=root&charset=utf8"
+      
+  db_connection = SmartTrack::Database::Connection.new(db_url)
+  db_connection.rom.gateways[:default].use_logger(Logger.new($stdout)) if development?
 
-db_connection = SmartTrack::Database::Connection.new(db_url)
-db_connection.rom.gateways[:default].use_logger(Logger.new($stdout)) if development?
+  SmartTrack::Database::Container.register(:db_connection, db_connection)
+  SmartTrack::Database::Container.register(:rom, db_connection.rom)
+  SmartTrack::Database::Container.register(:sequel, db_connection.sequel)
+  SmartTrack::Database::Container.register(:user_repo, SmartTrack::Database::Repository::UserRepo.new(db_connection.rom))
+  SmartTrack::Database::Container.register(:session_repo, SmartTrack::Database::Repository::UserSessionRepo.new(db_connection.rom))
+end
+setup
 
-SmartTrack::Database::Container.register(:db_connection, db_connection)
-SmartTrack::Database::Container.register(:rom, db_connection.rom)
-SmartTrack::Database::Container.register(:sequel, db_connection.sequel)
 
 include SmartTrack::TokenAuth
 
 # Hooks
 before do
   @rom = SmartTrack::Database::Container.resolve(:rom)
-  
+  @user_repo = SmartTrack::Database::Container.resolve(:user_repo)
+  @session_repo = SmartTrack::Database::Container.resolve(:session_repo)
+
   req_body = request.body.read
   @payload = JSON.parse(req_body) unless req_body.empty?
 end
@@ -51,18 +64,17 @@ get '/protected' do
 end
 
 post '/login' do
-  user_repo = SmartTrack::Database::Repository::UserRepo.new(@rom)
-  session_repo = SmartTrack::Database::Repository::UserSessionRepo.new(@rom)
-  user = user_repo.find_by_email(@payload['email'])
+  user = @user_repo.find_by_email(@payload['email'])
 
   if user && password_matched(user.password, @payload['password'])
-    user_session = session_repo.find_by_user_id(user.id)
-    session_repo.delete(user_session.id) if user_session
+    user_session = @session_repo.find_by_user_id(user.id)
+    @session_repo.delete(user_session.id) if user_session
     
-    user_session = session_repo.create(
+    changeset = @session_repo.changeset(
       token: generate_session_token,
       user_id: user.id,
-      expired_at: Time.now + (60*60*24*30))
+      expired_at: Time.now + (60*60*24*30)).map(:add_timestamps)
+    user_session = @user_repo.create(changeset)
 
     return [200, json(token: user_session.token)]
   end
