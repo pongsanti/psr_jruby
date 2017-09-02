cab8e2121e2e791cb6fd721b7e8c298ce39c7f6f
pongsanti <pongsanti.tanvejsilp@gmail.com>
2017-09-02 22:35:33 +0700

    remove not used

--- spec/database/operation_spec.rb
+++ spec/database/operation_spec.rb
@@ -1,26 +1,26 @@
-require 'smarttrack'
-require_relative '../db_helper'
+# require 'smarttrack'
+# require_relative '../db_helper'
 
-describe SmartTrack::Database do
-  include SmartTrack::Test::Helper
+# describe SmartTrack::Database do
+#   include SmartTrack::Test::Helper
 
-  around(:each) do |example|
-    DB.db.transaction(rollback: :always, auto_savepoint: true) {example.run}
-  end
+#   around(:each) do |example|
+#     DB.db.transaction(rollback: :always, auto_savepoint: true) {example.run}
+#   end
 
-  context 'in user existed context' do
-    it 'can find user by email' do
-      create_user('jane@gmail.com')
+#   context 'in user existed context' do
+#     it 'can find user by email' do
+#       create_user('jane@gmail.com')
 
-      user = DB.find_user('jane@gmail.com')
-      expect(user).to_not be_nil
-    end
-  end
+#       user = DB.find_user('jane@gmail.com')
+#       expect(user).to_not be_nil
+#     end
+#   end
 
-  context 'in user not existed context' do
-    it 'cannot find user by email' do
-      user = DB.find_user('mockuser')
-      expect(user).to be_nil
-    end
-  end
-end
+#   context 'in user not existed context' do
+#     it 'cannot find user by email' do
+#       user = DB.find_user('mockuser')
+#       expect(user).to be_nil
+#     end
+#   end
+# end
