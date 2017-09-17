a887d8b9637c93b596956c2c39ac381d20a0c579
pongsanti <pongsanti.tanvejsilp@gmail.com>
2017-09-17 15:22:09 +0700

    PATCH for CORS

--- lib/smarttrack/constant.rb
+++ lib/smarttrack/constant.rb
@@ -1,7 +1,7 @@
 module SmartTrack::Constant
   CORS_HASH = {
     'Access-Control-Allow-Origin' => '*',
-    'Access-Control-Allow-Methods' => 'HEAD,GET,PUT,POST,DELETE,OPTIONS',
+    'Access-Control-Allow-Methods' => 'HEAD,GET,PATCH,POST,DELETE,OPTIONS',
     'Access-Control-Allow-Headers' => 'X-Authorization, Content-Type' }
 
   ONE_MONTH_IN_MS = (60*60*24*30)
