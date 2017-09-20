de6ff9de515182e0af8ba5a5ef0251e063e7aef4
pongsanti <pongsanti.tanvejsilp@gmail.com>
2017-09-20 15:18:35 +0700

    user staions belongs to

--- lib/smarttrack/database/relations/user_stations.rb
+++ lib/smarttrack/database/relations/user_stations.rb
@@ -2,7 +2,8 @@ module SmartTrack::Database::Relations
   class UserStations < ROM::Relation[:sql]
     schema(:user_stations, infer: true) do
       associations do
-        
+        belongs_to :user
+        belongs_to :gps_test__tblstation
       end
     end
   end
