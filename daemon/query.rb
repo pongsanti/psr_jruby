274874adc60d0adbbe24defb8cdcd030460b0a1c
pongsanti <pongsanti.tanvejsilp@gmail.com>
2017-10-01 02:28:21 +0700

    uncomment condition of location query

--- daemon/query.rb
+++ daemon/query.rb
@@ -16,7 +16,7 @@ def locations_by_plates(db, plates)
     .left_join(:tblcarsets, vid: :vid)
     .where(plate: plates)
     .order(Sequel.desc(:gps_datetime))
-    #.where { gps_datetime >= now - (3 * MINUTE) }
+    .where { gps_datetime >= now - (3 * MINUTE) }
   
     location_plates = Set.new
     locations = []
